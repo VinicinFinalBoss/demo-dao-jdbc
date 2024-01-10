68f1e89f69cfd99f3783d7438937d77159abfc25
Vinicius Lima <viniciuslima940@gmail.com>
2024-01-10 18:34:51 -0300

    Criado classe entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(19, "Vinícius", "viniciuslima940@gmail.com",new Date(), 5999.9, obj );
+		System.out.println(seller);
 	}
 }
