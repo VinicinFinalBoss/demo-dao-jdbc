e2b9072e4ebc73cf191d085a70538557efe97b9c
Vinicius Lima <viniciuslima940@gmail.com>
2024-01-11 12:20:56 -0300

    FindById() implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,16 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(19, "Vinícius", "viniciuslima@gmail.com",new Date(), 5999.9, obj );
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable{
-	
 	private static final long serialVersionUID = 1L;
 	private Integer Id;
 	private String name;
