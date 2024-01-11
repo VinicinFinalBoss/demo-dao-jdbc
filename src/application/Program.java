ff2a0e7bc8b9184d3c31161b986e5c13781fb3df
Vinicius Lima <viniciuslima940@gmail.com>
2024-01-11 17:35:05 -0300

    Feita implementação FindAll()

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,9 +14,15 @@ public class Program {
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		System.out.println("=== TEST 2: seller findByDepartment ===");
+		System.out.println("\n=== TEST 2: seller findByDepartment ===");
 		Department dp = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(dp);
+		
+		for(Seller obj: list) {
+			System.out.println(obj);
+		}
+		System.out.println("\n=== TEST 3: seller findByDepartment ===");
+		list = sellerDao.findAll();
 		
 		for(Seller obj: list) {
 			System.out.println(obj);
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -87,8 +87,40 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT seller.*, department.Name as DepName "
+					+ "FROM seller "
+					+ "INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List<Seller> sellers = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {
+				Department objDep = map.get(rs.getInt("DepartmentId"));
+				if (objDep == null) {
+					 objDep = instantiateDepartment(rs);
+					 map.put(rs.getInt("DepartmentId"), objDep);
+				}
+				
+				
+				Seller objSeller = instantiateSeller(rs, objDep);
+				sellers.add(objSeller);
+				
+			}return sellers;
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
