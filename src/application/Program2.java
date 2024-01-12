43b68fc1389513ccb5f6e326b14466ace7e57826
Vinicius Lima <viniciuslima940@gmail.com>
2024-01-11 23:46:09 -0300

    Implementação do findAll() departments

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.sql.Connection;
+import java.util.List;
 
 import db.DB;
 import model.dao.DaoFactory;
@@ -16,14 +17,20 @@ public class Program2 {
 		depDao.insert(dep);
 		System.out.println("===== Test 1: Insert Department ===== ");
 		System.out.println("Inserted new Department "+ dep.getId());
-		System.out.println("===== Test 2: FindById Department ===== ");
+		System.out.println("\n===== Test 2: FindById Department ===== ");
 		dep = depDao.findById(4);
 		System.out.println(dep);
-		System.out.println("===== Test 3: Update Department ===== ");
+		System.out.println("\n===== Test 3: Update Department ===== ");
 		dep = depDao.findById(2);
 		dep.setName("Style");
 		depDao.update(dep);
 		System.out.println("Updated!");
+		System.out.println("\n===== Test 4: FindAll Departments ===== ");
+		List<Department> deps = depDao.findAll();
+		for(Department department: deps) {
+			System.out.println(department);
+		}
+		
 		
 		
 		
